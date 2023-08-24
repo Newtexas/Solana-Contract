64ef0203de08add5614e7d58b82e3955ea250aa7
Newtexas <info@new-texas.com>
2023-08-24 08:04:38 +0000

    second commit

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import logo from './assets/NTlogos/SocialMedia-Icon.png';
 import './App.css';
+import GetSupply from './components/API/GetSupply';
+import GetTokenSupply from './components/API/GetTokenSupply';
+import Test2 from './components/Test2';
+import Walletconnection from './components/Connection/Walletconnection';
+import { ContentBoxFrame } from './styles/Frame';
+import { GlobalStyles } from "./styles/Global";
+import Setglobal from './styles/Setglobal';
+import Dummy from './components/Dummy';
+import { DataProvider } from './context/Dataprovider';
+import SPLtoken from './components/SPLtoken';
+import Dummy2 from './components/Dummy2';
+import Dummy3 from './components/Dummy3';
+import Rpctest from './components/Rpctest.js';
+import Chaindata from './components/Contract/Chaindata';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Setglobal />
+          <GlobalStyles />
+          <DataProvider>
+            <ContentBoxFrame>
+              <Walletconnection />
+              <Chaindata/>
+              <SPLtoken />
+            </ContentBoxFrame>
+          </DataProvider>
+        </header>
+      </div>
+    </>
   );
 }
 
